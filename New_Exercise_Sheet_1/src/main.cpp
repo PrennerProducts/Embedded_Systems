c5a09fa47cc09093b9d6c867be23e1d0d382a685
PrennerProducts <61316223+PrennerProducts@users.noreply.github.com>
2022-06-16 20:41:30 +0200

    Exercise_Sheet_2

---
+++ New_Exercise_Sheet_1/src/main.cpp
@@ -0,0 +1,15 @@
+//
+// Created by McRebel on 01.06.2022.
+//
+
+/* and.c */
+#include "bitoperations.h"
+#include <avr/io.h>
+#include "bitoperations.h"
+
+#include <stdlib.h>
+
+
+int main() {
+
+}
